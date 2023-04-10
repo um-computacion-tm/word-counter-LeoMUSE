d30deb2b146089effc9ce8d65c056e79287e7645
Leandro Flores <leandroflores@pop-os.localdomain>
2023-04-10 14:24:12 -0300

    modifique los test

--- TestCountWords.py
+++ TestCountWords.py
@@ -5,20 +5,23 @@ from CountWords import countWords
 class TestTrabajo2(unittest.TestCase):
 
     def test_1(self):
-        frase = "hola hola como estas"
-        self.assertEqual(countWords(frase), {"hola": 2, "como": 1, "estas": 1})
+        resultado = countWords("hola hola como estas")
+        self.assertEqual(resultado, {"hola": 2, "como": 1, "estas": 1})
 
     def test_2(self):
-        frase = "Como hola como Como si estas"
-        self.assertEqual(countWords(frase), {"hola": 1, "como": 3, "estas": 1, "si": 1})
+        resultado = countWords("Como hola como Como si estas")
+        self.assertEqual(resultado, {"hola": 1, "como": 3, "estas": 1, "si": 1})
 
     def test_3(self):
-        frase = "Como como Como como como"
-        self.assertEqual(countWords(frase), {"como": 5})
+        resultado = countWords("Como como Como como como")
+        self.assertEqual(resultado, {"como": 5})
 
     def test_4(self):
-        frase = "habia un dia que Un Perro le Ladro a otro otro perro el Perro ladro"
-        self.assertEqual(countWords(frase), {"habia": 1, "un": 2, "dia": 1, "que": 1, "perro": 3, "le": 1, "ladro": 2, "a": 1, "otro": 2, "el": 1})
+        resultado = countWords("habia un dia que Un Perro le Ladro a otro otro perro el Perro ladro")
+        self.assertEqual(resultado, {"habia": 1, "un": 2, "dia": 1, "que": 1, "perro": 3, "le": 1, "ladro": 2, "a": 1, "otro": 2, "el": 1})
 
+    def test_5(self):
+        resultado = countWords("hola Hola Como como Estas estas perro casa auto")
+        self.assertEqual(resultado, {"hola": 2, "como": 2,"estas": 2,"perro": 1,"casa": 1,"auto": 1})
 if __name__ == "__main__":
     unittest.main()
